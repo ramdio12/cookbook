350c738af369333e8df845232d0ccb16e820558b
ramdio12 <luib.diomar@gmail.com>
2023-12-12 20:27:13 +0800

    some files were removed (unnecessary files)

--- src/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Login from "./pages/Login";
-
-test("render login component in the document", () => {
-  const component = render(<Login />);
-  console.log(component);
-});
