24dbc157dffa03d3a40337b17ed0020fc8666f86
ramdio12 <luib.diomar@gmail.com>
2023-12-13 11:50:33 +0800

    NotFound Component Edited

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,7 +2,7 @@ import notfoundlogo from "../assets/notfound.png";
 
 const NotFound = () => {
   return (
-    <div className="text-6xl flex items-center justify-center h-screen flex-col">
+    <div className="text-6xl flex items-center justify-center h-screen flex-col text-center">
       <img src={notfoundlogo} alt="logo" width={500} />
       <h1 className="font-bold">ERROR NO. 404 </h1>
       <h2>Requested Page Not Found</h2>
